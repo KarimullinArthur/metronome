5f5eb4e5df187268f13a87d7a7b4db9990536696
Arthur <arthurkarimullin2005@gmail.com>
2022-02-18 18:18:52 +0500

    исключения

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import time
 
-bpm = int(input('BPM = '))
-bpm = 60 / bpm
-while True:
-    print('\a',end='\rBeep')
-    time.sleep(bpm)
+try:
+	bpm = int(input('BPM = '))
+	bpm = 60 / bpm
+	while True:
+		print('\a',end='\rBeep')
+		time.sleep(bpm)
+except ValueError:
+	print('What?')
+
+except KeyboardInterrupt:
+	print('\nStop!')
